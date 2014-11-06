a84b31cbd79b1d81fdb7cffc207a7dbd81e9ee46
chncho <chncho07@gmail.com>
2014-11-07 00:28:36 +0800

    终于提交成功

---
+++ src/com/baidu/ueditor/upload/Uploader.java
@@ -0,0 +1,35 @@
+package com.baidu.ueditor.upload;
+
+import com.baidu.ueditor.ActionEnter;
+import com.baidu.ueditor.define.State;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+public class Uploader {
+	Logger logger = Logger.getLogger(ActionEnter.class);
+	private HttpServletRequest request = null;
+	private Map<String, Object> conf = null;
+
+	public Uploader(HttpServletRequest request, Map<String, Object> conf) {
+		this.request = request;
+		this.conf = conf;
+	}
+
+	public final State doExec() {
+		String filedName = (String) this.conf.get("fieldName");
+		State state = null;
+
+		if ("true".equals(this.conf.get("isBase64"))) {
+			state = Base64Uploader.save(this.request.getParameter(filedName),
+					this.conf);
+		} else {
+			state = BinaryUploader.save(this.request, this.conf);
+		}
+
+		return state;
+	}
+}
